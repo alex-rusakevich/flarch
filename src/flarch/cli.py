06206ce46ba599fa4d447198fcddbc18f2f8b47d
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-10-31 19:02:29 +0300

    #patch show program's version

--- src/flarch/cli.py
+++ src/flarch/cli.py
@@ -5,10 +5,16 @@ from flarch.commands.nullify_drive import nullify_drive
 from flarch.commands.write_image import write_image
 from flarch.utils.commands_exist import ensure_commands_exist
 
+from flarch.__about__ import __version__
+
 
 def main():
     parser = argparse.ArgumentParser(description="Work with floppies with ease!")
     subparsers = parser.add_subparsers(title="commands", dest="command")
+
+    parser.add_argument(
+        "-v", "--version", action="store_true", help="Show program's version and exit"
+    )
 
     # region Format floppy
     format_parser = subparsers.add_parser("format", help="Format the floppy into FAT")
@@ -50,20 +56,27 @@ def main():
 
     ensure_commands_exist(["blockdev", "mkfs.vfat", "dd", "pv"])
 
+    if args.version:
+        print(__version__)
+        return
+
     if args.command == "nullify":
         nullify_drive(args.drive)
 
-    if args.command == "format":
+    elif args.command == "format":
         if not args.skip_nullification:
             nullify_drive(args.drive)
 
         format_drive(args.drive)
 
-    if args.command == "write":
+    elif args.command == "write":
         if not args.skip_nullification:
             nullify_drive(args.drive_to)
 
         write_image(image_path=args.image_from, drive=args.drive_to)
+
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
